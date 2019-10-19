53ee78b51922821926dedf3aee15770e86c4ebd1
Gonzalo SR <gonzalo@gonzalosr.com>
2019-10-19 13:33:08 +0200

    added aws-console-color script

--- static-account-switch-aws.user.js
+++ static-account-switch-aws.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Static AWS account switch
 // @namespace    static.account.switch.aws.gonzalosr.com
-// @version      0.2
+// @version      0.3
 // @description  Set a group of accounts and roles as static in the AWS switch role history
 // @author       Gonzalo Sanchez Romero
 // @match        https://*console.aws.amazon.com/*
@@ -67,5 +67,38 @@
     finalHTML = finalHTML.concat(`<div class="awsc-account-display-section awsc-username-menu-section"></div>`);
     let mySwitches=document.getElementById('awsc-username-menu-recent-roles');
     mySwitches.insertAdjacentHTML('beforeend',finalHTML);
+
+    // Rest of this script Copyright 2018 Tom Bamford. https://github.com/manicminer/userscript-aws-console-colors
+    let s = 1.0, l = 0.25;
+
+    function determineNewColor(rgbColor) {
+        let f = rgbColor.split(","), r = parseInt(f[0].slice(4)), g = parseInt(f[1]), b = parseInt(f[2]);
+        r /= 255; g /= 255; b /= 255;
+        let h, max = Math.max(r, g, b), min = Math.min(r, g, b);
+        if (max == min) {
+            h = s = 0; // achromatic
+        } else {
+            let d = max - min;
+            switch (max) {
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+        return 'hsl(' + Math.floor(h * 360) + ',' + Math.floor(s * 100) + '%,' + Math.floor(l * 100) + '%)';
+    }
+
+    let switcherClass = 'awsc-switched-role-username-wrapper';
+    let roleElems = document.getElementsByClassName(switcherClass);
+    if (roleElems.length == 1) {
+        let bgColor = roleElems[0].style.backgroundColor;
+        let newBgColor = determineNewColor(bgColor);
+        let navSelector = '#nav-menubar, #nav-menu-right, .nav-menu, .nav-menu-separator';
+        let menuBarElems = document.querySelectorAll(navSelector);
+        for (let i = 0; i < menuBarElems.length; i++) {
+            menuBarElems[i].style.backgroundColor = newBgColor;
+        }
+    }
 
 })();
